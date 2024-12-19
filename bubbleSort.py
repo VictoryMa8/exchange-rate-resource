ec1ae90edebc3bd3adda2658150f5feb857a6882
Victor <160278295+VictoryMa8@users.noreply.github.com>
2024-12-18 19:24:00 -0600

    Clean up

--- bubbleSort.py
+++
@@ -1,8 +0,0 @@
-def main():
-    array1 = [10, 8, 32, 25, 1, 92, 40, 51, 7]
-    print(array1)
-    name = input("Enter your name: ")
-    print("Hello, " + name)
-
-if __name__ == '__main__':
-    main()
